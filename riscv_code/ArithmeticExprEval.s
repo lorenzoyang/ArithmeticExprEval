3a089d3c1b03a50b82df5658718e47d94b1bfa0a
Yang <yangrongwei2002@gmail.com>
2024-04-27 16:12:24 +0200

    riscv: implemented Subtraction, added SubtractionTest

--- riscv_code/ArithmeticExprEval.s
+++ riscv_code/ArithmeticExprEval.s
@@ -8,7 +8,7 @@ input: .string "1+2"
 
 # Tipi di errore >>>
 
-state: .word 0 # indica lo stato del programma
+state: .word 0 # indica lo stato del programma, 0 = nessun errore
 syntaxError: .word 1 # errore di sintassi
 divisionByZeroError: .word 2 # divisione per lo zero
 overflowError: .word 3 # errore di overflow
@@ -25,7 +25,7 @@ parenthesesErrorMsg: .string "errore di prentesi"
 
 # <<<
 
-# Costanti globali >>>
+# Costanti >>>
 
 INT32_MIN: .word -2147483648
 INT32_MAX: .word 2147483647
@@ -42,7 +42,7 @@ main:
 # Operazioni aritmetiche >>>
 
 # Function: Addition
-#     a0: il risultato della somma
+#     a0: risultato dell'addizione
 #     a1(a), a2(b): addendi
 Addition:
     # t0 = INT32_MIN
@@ -51,32 +51,72 @@ Addition:
     addi sp, sp, -4
     sw ra, 0(sp)
         
-    bgtz a2, positive_b
+    bgtz a2, positive_b_Addition
     
     lw t0, INT32_MIN
     sub t0, t0, a2 # t0 = INT32_MIN - b
-    blt a1, t0 overflow_error
+    blt a1, t0 overflow_error_Addition
     j add_operation
     
-    positive_b:
+    positive_b_Addition:
         lw t1, INT32_MAX
         sub t1, t1, a2 # t1 = INT32_MAX - b
-        blt t1, a1 overflow_error
+        blt t1, a1 overflow_error_Addition
     add_operation:
         add a0, a1, a2
-        j Addition_end
+        j end_Addition
 
-    overflow_error:
+    overflow_error_Addition:
         la t2, state
         lw t3, overflowError
         sw t3, 0(t2)
         mv a0, zero 
-    Addition_end:
+    end_Addition:
         lw ra, 0(sp)
         addi sp, sp, 4
         ret
+# End
 
+
+
+# Function: Subtraction
+#     a0: risultato della sottrazione
+#     a1(a): minuendo
+#     a2(b): sottraendo
 Subtraction:
+    # t0 = INT32_MIN
+    # t1 = INT32_MAX
+    
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    
+    bgtz a2, positive_b_Subtraction
+    
+    lw t1, INT32_MAX
+    add t1, t1, a2 # t1 = t1 + b
+    blt t1, a1 overflow_error_Subtraction # t1 < a
+    j sub_operation
+    
+    positive_b_Subtraction:
+        lw t0, INT32_MIN
+        add t0, t0, a2 # t0 = t0 + b
+        blt a1, t0 overflow_error_Subtraction
+    sub_operation:
+        sub a0, a1, a2
+        j end_Subtraction
+        
+    overflow_error_Subtraction:
+        la t2, state
+        lw t3, overflowError
+        sw t3, 0(t2)
+        mv a0, zero 
+    end_Subtraction:
+        lw ra, 0(sp)
+        addi sp, sp, 4
+        ret
+# End
+
+
     
 Multiplication:
     
--- riscv_code/SubtractionTest.s
+++ riscv_code/SubtractionTest.s
@@ -1,4 +1,4 @@
-.data 
+.data
 
 state: .word 0 # indica lo stato del programma
 
@@ -12,9 +12,9 @@ INT32_MAX: .word 2147483647
 
 test:
     li a1, -2147483648
-    li a2, 669
+    li a2, 2
     
-    jal Addition
+    jal Subtraction
     
     lw t0, state
     lw t1, overflowError
@@ -32,41 +32,44 @@ test:
     end:
         li a7 10
         ecall
-    
 
 
-# Function: Addition
-#     a0: risultato dell'addizione
-#     a1(a), a2(b): addendi
-Addition:
+
+# Function: Subtraction
+#     a0: risultato della sottrazione
+#     a1(a): minuendo
+#     a2(b): sottraendo
+Subtraction:
     # t0 = INT32_MIN
     # t1 = INT32_MAX
     
     addi sp, sp, -4
     sw ra, 0(sp)
+    
+    bgtz a2, positive_b_Subtraction
+    
+    lw t1, INT32_MAX
+    add t1, t1, a2 # t1 = t1 + b
+    blt t1, a1 overflow_error_Subtraction # t1 < a
+    j sub_operation
+    
+    positive_b_Subtraction:
+        lw t0, INT32_MIN
+        add t0, t0, a2 # t0 = t0 + b
+        blt a1, t0 overflow_error_Subtraction
+    sub_operation:
+        sub a0, a1, a2
+        j end_Subtraction
         
-    bgtz a2, positive_b_Addition
-    
-    lw t0, INT32_MIN
-    sub t0, t0, a2 # t0 = INT32_MIN - b
-    blt a1, t0 overflow_error_Addition
-    j add_operation
-    
-    positive_b_Addition:
-        lw t1, INT32_MAX
-        sub t1, t1, a2 # t1 = INT32_MAX - b
-        blt t1, a1 overflow_error_Addition
-    add_operation:
-        add a0, a1, a2
-        j end_Addition
-
-    overflow_error_Addition:
+    overflow_error_Subtraction:
         la t2, state
         lw t3, overflowError
         sw t3, 0(t2)
         mv a0, zero 
-    end_Addition:
+    end_Subtraction:
         lw ra, 0(sp)
         addi sp, sp, 4
         ret
 # End
+        
+    
